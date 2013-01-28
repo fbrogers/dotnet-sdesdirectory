58592898d76578390639fb24d04e555d60392669
Jordan Rogers <fbrogers@gmail.com>
2013-01-28 16:58:38 -0500

    small updates

--- Models/DirectoryModel.Designer.cs
+++ Models/DirectoryModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Default code generation is disabled for model 'C:\TFSProjects\SDES - Office Directory\1. Development\Models\DirectoryModel.edmx'. 
+﻿// Default code generation is disabled for model 'C:\TFSProjects\SDES - Office Directory\2. Testing\Models\DirectoryModel.edmx'. 
 // To enable default code generation, change the value of the 'Code Generation Strategy' designer
 // property to an alternate value. This property is available in the Properties Window when the model is
 // open in the designer.
